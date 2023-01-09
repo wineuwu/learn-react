537721690e83fcbf25498dc9119fa70759a887e6
Winnie <asgh03226@gmail.com>
2023-01-09 17:16:38 +0800

    練習useState

--- src/component/Button.jsx
+++ src/component/Button.jsx
@@ -3,7 +3,10 @@ import { useState } from 'react';
 const CountButton =(props) => { 
 
   const [count, setCount] = useState(0);
-  const handleClick = () => setCount(count +1);
+  const handleClick = () =>{ 
+    setCount(count +1)
+    props.handelSum(1);
+  };
 
   return (
     <button className="border  rounded-md p-3 w-40 inline-block" onClick={handleClick}>
